acc7f4a53c65dbfd823ceb02a471a6743017e594
Erwan Richard <erwann.richard@gmail.com>
2013-03-14 10:23:24 +0100

    Make the plugin work when multiple namespace are found

--- PHPCompanion.py
+++ PHPCompanion.py
@@ -1,82 +1,94 @@
-import sublime, sublime_plugin, re, mmap, contextlib, os
+import sublime
+import sublime_plugin
+import re
+import mmap
+import contextlib
+import os
 
 setting = sublime.load_settings('PHP Companion.sublime-settings').get
 
+def find_symbol(symbol, window):
+    files = window.lookup_symbol_in_index(symbol)
+    namespaces = []
+    pattern = re.compile(b'namespace\s+([^;]+);')
+
+    def filter_file(file):
+        if setting('exclude_dir'):
+            for dir in setting('exclude_dir'):
+                if dir in file[1]:
+                    return False
+
+        return file
+
+    for file in files:
+        if filter_file(file):
+            with open(file[0], "r+b") as f:
+                with contextlib.closing(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)) as m:
+                    for match in re.findall(pattern, m):
+                        namespaces.append([match.decode('utf-8') + "\\" + symbol, file[1]])
+                        break
+
+    return namespaces
+
+
 class ImportUseCommand(sublime_plugin.TextCommand):
+    def run(self, edit, namespace):
+        view = self.view
+        use_stmt = "use " + namespace + ";"
+
+        region = view.find(use_stmt.replace('\\','\\\\'), 0)
+        if not region.empty():
+            return sublime.status_message('Use already exist !')
+
+        uses = []
+        regions = view.find_all(r"^\s*(use\s+[\w\\]+[;])", 0, '$1', uses)
+        uses.append(use_stmt)
+        uses = list(set(uses))
+        uses.sort()
+        uses = "\n"+"\n".join(uses)
+
+
+        if len(regions) > 0:
+            region = regions[0]
+            for r in regions:
+                region = region.cover(r)
+
+            view.replace(edit, region, uses)
+            return True
+
+        region = view.find(r"^\s*namespace\s+[\w\\]+[;{]", 0)
+        if not region.empty():
+            line = view.line(region)
+            view.insert(edit, line.end(), uses)
+            return True
+
+        region = view.find(r"<\?php", 0)
+        if not region.empty():
+            line = view.line(region)
+            view.insert(edit, line.end(), uses)
+            return True
+
+class FindUseCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         symbol = view.substr(view.word(view.sel()[0]))
 
-        if re.match(r"\w", symbol) == None:
+        if re.match(r"\w", symbol) is None:
             return sublime.status_message('Not a valid symbol "%s" !' % symbol)
 
-        exclude_dir = setting('exclude_dir')
+        self.namespaces = find_symbol(symbol, view.window())
 
-        files = view.window().lookup_symbol_in_index(symbol)
-        namespaces = []
-        pattern = re.compile(rb'namespace\s+([^;]+);')
+        if len(self.namespaces) == 1:
+            self.view.run_command("import_use", {"namespace": self.namespaces[0][0]})
 
-        def filter_file(file):
-            if setting('exclude_dir'):
-                for dir in setting('exclude_dir'):
-                    if dir in file[1]:
-                        return False
-            
-            return file
-            
-        for file in files:
-            if filter_file(file):
-                with open(file[0], "r+b") as f:
-                    with contextlib.closing(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)) as m:
-                        for match in re.findall(pattern,m):
-                            namespaces.append(match.decode('utf-8') + "\\" + symbol)
-                            break
-            
-        def import_use_in_view(view, edit, namespace):
-            uses = []
-            region = view.find_all(r"^\s*(use\s+[\w\\]+[;])", 0, '$1', uses)
-            uses.append("use " + namespace + ";")
-            uses = list(set(uses))
-            uses.sort()
+        if len(self.namespaces) > 1:
+            view.window().show_quick_panel(self.namespaces, self.on_done)
 
-            print(edit)
+    def on_done(self, index):
+        if index == -1:
+            return
 
-            if len(region) > 0:
-                region = sublime.Region(region[0].begin(),region.pop().end())
-                view.replace(edit, region, "\n"+"\n".join(uses))
-                return True
-
-            region = view.find(r"^\s*namespace\s+[\w\\]+[;{]", 0)
-            if not region.empty():
-                line = view.line(region)
-                line_contents = '\n\n' + "use " + namespace + ";"
-                view.insert(edit, line.end(), line_contents)
-                return True
-
-            region = view.find(r"<\?php", 0)
-            if not region.empty():
-                line = view.line(region)
-                line_contents = '\n\n' + "use " + namespace + ";"
-                view.insert(edit, line.end(), line_contents)
-                return True
-        
-        def on_select(index):
-            import_use_in_view(self.view, edit, namespaces[index])
-            return True
-
-        if len(namespaces) == 1:
-            import_use_in_view(view, edit, namespaces[0])
-            return True
-
-        if len(namespaces) > 1:
-            view.window().show_quick_panel(namespaces, on_select, sublime.MONOSPACE_FONT)
-
-        return True 
-
-
-
-
-
+        self.view.run_command("import_use", {"namespace": self.namespaces[index][0]})
 
 class ImportNamespaceCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -105,7 +117,7 @@ class ImportNamespaceCommand(sublime_plugin.TextCommand):
         namespaceStmt = re.sub('/', '\\\\', namespaceStmt)
 
         region = self.view.find(r"<\?php", 0)
-        if region != None:
+        if not region.empty():
             line = self.view.line(region)
             line_contents = '\n\n' + "namespace " + namespaceStmt + ";"
             self.view.insert(edit, line.end(), line_contents)
