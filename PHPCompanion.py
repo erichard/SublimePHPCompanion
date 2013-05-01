230059be4318f2a824cc6d4b8aab1bf8247bf9cc
Erwan Richard <erwann.richard@gmail.com>
2013-05-01 17:44:21 +0200

    Fix the expand to FQCN
    Fix an issue when the class is find multiple times across the project.

--- PHPCompanion.py
+++ PHPCompanion.py
@@ -118,9 +118,10 @@ class ExpandFqcnCommand(sublime_plugin.TextCommand):
             return sublime.status_message('Not a valid symbol "%s" !' % symbol)
 
         self.namespaces = find_symbol(symbol, view.window())
+        self.leading_separator = leading_separator;
 
         if len(self.namespaces) == 1:
-            self.view.run_command("replace_fqcn", {"region_start": self.region.begin(), "region_end": self.region.end(), "namespace": self.namespaces[0][0], "leading_separator": leading_separator})
+            self.view.run_command("replace_fqcn", {"region_start": self.region.begin(), "region_end": self.region.end(), "namespace": self.namespaces[0][0], "leading_separator": self.leading_separator})
 
         if len(self.namespaces) > 1:
             view.window().show_quick_panel(self.namespaces, self.on_done)
@@ -129,7 +130,7 @@ class ExpandFqcnCommand(sublime_plugin.TextCommand):
         if index == -1:
             return
 
-        self.view.run_command("replace_fqcn", {"region_start": self.region.begin(), "region_end": self.region.end(), "namespace": self.namespaces[index][0], "leading_separator": leading_separator})
+        self.view.run_command("replace_fqcn", {"region_start": self.region.begin(), "region_end": self.region.end(), "namespace": self.namespaces[index][0], "leading_separator": self.leading_separator})
 
 
 class ImportNamespaceCommand(sublime_plugin.TextCommand):
