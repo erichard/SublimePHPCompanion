e644b19d5ef8d0e9c4554729034bbbc5402c80d9
chadrien <chadrien@chadrien.fr>
2013-07-13 15:04:13 +0200

    Refoctored ImportUseCommand (extract methods)

--- php_companion/commands/import_use_command.py
+++ php_companion/commands/import_use_command.py
@@ -3,36 +3,51 @@ import sublime_plugin
 
 class ImportUseCommand(sublime_plugin.TextCommand):
     def run(self, edit, namespace):
-        view = self.view
-        use_stmt = "use " + namespace + ";"
+        self.namespace = namespace
 
-        region = view.find(use_stmt.replace('\\', '\\\\'), 0)
-        if not region.empty():
+        if self.is_already_used():
             return sublime.status_message('Use already exist !')
 
-        uses = []
-        regions = view.find_all(r"^(use\s+.+[;])", 0, '$1', uses)
-        uses.append(use_stmt)
-        uses = list(set(uses))
-        uses.sort()
-        uses = "\n".join(uses)
+        self.insert_use(edit)
 
+    def insert_use(self, edit):
+        if self.is_first_use():
+            for location in [r"^\s*namespace\s+[\w\\]+[;{]", r"<\?php"]:
+                self.insert_first_use(location, edit)
+        else:
+            self.insert_use_among_others(edit)
+
+    def insert_first_use(self, where, edit):
+        region = self.view.find(where, 0)
+        if not region.empty():
+            line = self.view.line(region)
+            self.view.insert(edit, line.end(), "\n" + self.build_uses())
+            sublime.status_message('Successfully imported' + self.namespace)
+
+    def insert_use_among_others(self, edit):
+        regions = self.view.find_all(r"^(use\s+.+[;])", 0)
         if len(regions) > 0:
             region = regions[0]
             for r in regions:
                 region = region.cover(r)
 
-            view.replace(edit, region, uses)
-            return sublime.status_message('Successfully imported' + namespace)
+            self.view.replace(edit, region, self.build_uses())
+            sublime.status_message('Successfully imported' + self.namespace)
 
-        region = view.find(r"^\s*namespace\s+[\w\\]+[;{]", 0)
-        if not region.empty():
-            line = view.line(region)
-            view.insert(edit, line.end(), "\n" + uses)
-            return sublime.status_message('Successfully imported' + namespace)
+    def build_uses(self):
+        uses = []
+        use_stmt = "use " + self.namespace + ";"
 
-        region = view.find(r"<\?php", 0)
-        if not region.empty():
-            line = view.line(region)
-            view.insert(edit, line.end(), "\n" + uses)
-            return sublime.status_message('Successfully imported' + namespace)
+        self.view.find_all(r"^(use\s+.+[;])", 0, '$1', uses)
+        uses.append(use_stmt)
+        uses = list(set(uses))
+        uses.sort()
+
+        return "\n".join(uses)
+
+    def is_already_used(self):
+        region = self.view.find(("use " + self.namespace + ";").replace('\\', '\\\\'), 0)
+        return not region.empty()
+
+    def is_first_use(self):
+        return len(self.view.find_all(r"^(use\s+.+[;])", 0)) == 0
