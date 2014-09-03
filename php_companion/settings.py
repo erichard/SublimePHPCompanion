3c9c36401205a891e2b3fad76b79b8e553714d69
Ronan <ronan@d3r.com>
2014-09-03 13:33:44 +0100

    Namespace prefixes and project settings
    Add get_setting() to settings.py and allow looking up config values in project data
    Adjust import_namespace_command.py to use get_setting() and to add a prefix to namespaces if defined
    Adjust utils.py to use get_setting()
    
    Signed-off-by: Ronan <ronan@d3r.com>

--- php_companion/settings.py
+++ php_companion/settings.py
@@ -1,2 +1,11 @@
+import sublime
+
 def filename():
     return 'PHP Companion.sublime-settings'
+
+def get_setting(name, default=None):
+    v = sublime.active_window().project_data().get("phpcompanion", {}).get(name, None)
+    if v != None:
+        return v
+    else:
+        return sublime.load_settings(filename()).get(name, default)
