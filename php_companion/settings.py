599bc171699869438e50fcb20411fdaed7641dbf
Erwan Richard <erwann.richard@gmail.com>
2015-07-01 17:49:45 +0200

    Fix project_data getter

--- php_companion/settings.py
+++ php_companion/settings.py
@@ -4,8 +4,13 @@ def filename():
     return 'PHP Companion.sublime-settings'
 
 def get_setting(name, default=None):
-    v = sublime.active_window().project_data().get("phpcompanion", {}).get(name, None)
-    if v != None:
-        return v
+    project_data = sublime.active_window().project_data();
+
+    if (project_data != None):
+        v = project_data.get("phpcompanion", {}).get(name, None)
+        if v != None:
+            return v
+        else:
+            return sublime.load_settings(filename()).get(name, default)
     else:
-        return sublime.load_settings(filename()).get(name, default)
+        return None
